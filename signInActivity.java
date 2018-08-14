a0a80030808f1a142fc39108a3cbf91f8b7a2282
TheRealBiggySmalls <nsujecki@student.unimelb.edu.au>
2018-08-14 13:17:11 +1000

    First UI Commit
    The basic files I've whipped up as a demo UI.

---
+++ signInActivity.java
@@ -0,0 +1,13 @@
+package com.example.nic.oldtivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
