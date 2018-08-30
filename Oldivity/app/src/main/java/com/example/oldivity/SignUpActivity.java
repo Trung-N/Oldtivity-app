59e12af1cd84135d1de26314e07d89a5d28f1a2a
Teresa <tlieu@student.unimelb.edu.au>
2018-08-31 02:17:28 +1000

    signup button fixes

--- Oldivity/app/src/main/java/com/example/oldivity/SignUpActivity.java
+++ Oldivity/app/src/main/java/com/example/oldivity/SignUpActivity.java
@@ -7,7 +7,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,13 +15,13 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class SignUpActivity extends AppCompatActivity {
 
     private static final String TAG = "EmailPassword";
 
     private EditText tFirstName, tLastName, tEmail, tPassword;
-    private Button buttonOK;
     private FirebaseAuth mAuth;
 
     @Override
@@ -33,58 +32,47 @@ public class SignUpActivity extends AppCompatActivity {
         //Get Firebase auth instance
         mAuth = FirebaseAuth.getInstance();
 
-        //Views
+        //User Inputs
         tFirstName = (EditText) findViewById(R.id.tFirstName);
-        tLastName = (EditText)findViewById(R.id.tLastName);
+        tLastName = (EditText) findViewById(R.id.tLastName);
         tEmail = (EditText) findViewById(R.id.tEmail);
         tPassword = (EditText) findViewById(R.id.tPassword);
 
-        //Button
-        buttonOK = findViewById(R.id.buttonOK);
-
-        buttonOK.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!validateForm()) {
-                    return;
-                }
-
-                mAuth.createUserWithEmailAndPassword(tEmail.getText().toString(), tPassword.getText().toString())
-                        .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d(TAG, "createUserWithEmail:success");
-                                    Toast.makeText(SignUpActivity.this, "Authentication sucess.",
-                                            Toast.LENGTH_SHORT).show();
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    startActivity(new Intent(SignUpActivity.this, MainActivity.class));
-
-
-                                   // updateUI(user);
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(SignUpActivity.this, "Authentication failed.",
-                                            Toast.LENGTH_SHORT).show();
-
-                                    //updateUI(null);
-                                }
-
-                            }
-                        });
-
-            }
-
-        });
     }
 
-    //private  updateUI(FirebaseUser user) {
-   // }
+    public void OK(View view) {
+        if (!validateForm()) {
+            return;
+        }
+        //Create user entry in database with authenticator
+        mAuth.createUserWithEmailAndPassword(tEmail.getText().toString(), tPassword.getText().toString())
+                .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign up success
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(SignUpActivity.this, "Account successfully created!",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                    .setDisplayName(tFirstName.getText().toString() + " " + tLastName.getText().toString()).build();
+                            user.updateProfile(profileUpdates);
+                            startActivity(new Intent(SignUpActivity.this, MainActivity.class));
+                            // updateUI(user);
+                        } else {
+                            // If sign up fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(SignUpActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                             //updateUI(null);
+                            }
+                    }
+                 });
+    }
 
 //Check that all fields are filled
-//
     private boolean validateForm() {
         boolean valid = true;
         String email = tEmail.getText().toString().trim();
