b40de8d747e2b8e038e05954af2d5166fea56905
anita nseri <anaseri@student.unimelb.edu.au>
2018-09-02 11:14:49 +1000

    individual profile according to emails

--- Oldivity/app/src/main/java/com/example/oldivity/profile.java
+++ Oldivity/app/src/main/java/com/example/oldivity/profile.java
@@ -1,13 +1,47 @@
 package com.example.oldivity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class profile extends AppCompatActivity {
+    private TextView editText;
+    private static final String TAG = "profile";
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        editText = findViewById(R.id.displayName);
+        getUserProfile();
     }
+
+    public void getUserProfile() {
+        // [START get_user_profile]
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+
+            editText.setText(user.getEmail());
+
+        }
+    }
+
+    public void myEvents(View view) {
+
+    }
+
+    public void friendsList(View view) {
+
+    }
+
+    public void exploreEvents(View view) {
+
+    }
+
 }
