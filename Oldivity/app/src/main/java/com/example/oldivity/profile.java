82ddc3ba4bb6de8e789ecf6aafe98e525f0f6a92
Xinbo Sun <xinbos@student.unimelb.edu.au>
2018-09-12 16:07:41 +1000

    fix

--- Oldivity/app/src/main/java/com/example/oldivity/profile.java
+++ Oldivity/app/src/main/java/com/example/oldivity/profile.java
@@ -78,13 +78,14 @@ public class profile extends AppCompatActivity {
         Intent intent = new Intent(profile.this, ExploreEvents.class);
         startActivity(intent);
     }
-<<<<<<< HEAD
-    public void goToChats(View view){
+
+    public void goToChats(View view) {
         Intent intent = new Intent(profile.this, MainChatActivity.class);
-=======
+    }
+
     public void Call(View view) {
         Intent intent = new Intent(profile.this, call.class);
->>>>>>> UI
+
         startActivity(intent);
     }
 
