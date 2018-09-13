4e5552e2b1f800bd2ca9e99ef766c3639969eed9
Doan Minh Trung Nguyen <trungn1@student.unimelb.edu.au>
2018-09-14 01:19:45 +1000

    Get event geocode using event address, fixed json return format

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -1,32 +1,36 @@
 var admin = require('firebase-admin');
-
 var serviceAccount = require('./oldivity-cbf15-firebase-adminsdk-t1dur-d81fb8262c.json');
-
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: 'https://oldivity-cbf15.firebaseio.com/'
+});
+
+var NodeGeocoder = require('node-geocoder');
+var geocoder = NodeGeocoder({
+    provider: 'opencage',
+    apiKey: '761238e07064497cb24af3499d16709b'
 });
 
 module.exports.login = function(req,res){
     var db = admin.database();
     var ref = db.ref().child('events');
     var events = [];
-    ref.on("value", function(snapshot){
+    ref.once("value", function(snapshot){
         events = Object.entries(snapshot.val());
         eventDateFilter(events);
-        res.send(events);
+        eventGeocodes = eventGeocode(events,function(geocodes){
+            console.log(geocodes);
+            res.send(arrayToJson(events));
+        });
     });
 
 };
-
-
 
 function eventDateFilter(events){
     var currentDate = new Date();
     var eventSize = events.length;
     for (var i = eventSize-1; i >= 0; i--){
-        event = Object.entries(events[i][1]);
-        eventDate = event[0][1].split('/');
+        eventDate = events[i][1]['date'].split('/');
         eventDate[1]--;
         eventDate[0]++;
         eventDate = new Date(eventDate[2], eventDate[1], (eventDate[0]));
@@ -36,5 +40,32 @@ function eventDateFilter(events){
 
     }
     return events;
+}
+
+function arrayToJson(events){
+    eventsJson = {};
+    for(i = 0; i < events.length; i++){
+        eventsJson[events[i][0]] = events[i][1];
+    }
+    return eventsJson;
+}
+
+function eventGeocode(events, callback){
+    addresses = [];
+    for (var i = 0; i < events.length; i++){
+        addresses.push(events[i][1]['location']);
+    }
+    geocoder.batchGeocode(addresses, function (err, results) {
+        // Return an array of type {error: false, value: []}
+        var geocodes = [];
+        for (var j = 0; j < events.length; j++){
+            geo = Object.entries(Object.entries(results[j])[1][1][0]);
+            lat = geo[0][1];
+            lng = geo[1][1];
+            geocodes.push([lat, lng]);
+        }
+        callback(geocodes) ;
+    });
+
 
 }
