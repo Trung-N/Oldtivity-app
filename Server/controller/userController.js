ae84dbd41ba02b93d5e7aa65773470913e937ea6
Doan Minh Trung Nguyen <trungn1@student.unimelb.edu.au>
2018-09-13 21:36:38 +1000

    Filter events to future events only

--- Server/controller/userController.js
+++ Server/controller/userController.js
@@ -13,6 +13,7 @@ module.exports.login = function(req,res){
     var events = [];
     ref.on("value", function(snapshot){
         events = eventToArray(snapshot);
+        events = eventDateFilter(events);
         res.send(events);
     });
 
@@ -25,14 +26,32 @@ function eventToArray(snapshot) {
         var event = [];
         event.push(childSnapshot.key);
         var eventDetail = [];
-        eventDetail.push(childSnapshot.child('date'));
-        eventDetail.push(childSnapshot.child('description'));
-        eventDetail.push(childSnapshot.child('location'));
-        eventDetail.push(childSnapshot.child('title'));
+        eventDetail.push(childSnapshot.child('date').val());
+        eventDetail.push(childSnapshot.child('description').val());
+        eventDetail.push(childSnapshot.child('location').val());
+        eventDetail.push(childSnapshot.child('title').val());
         event.push(eventDetail);
-
         events.push(event);
     });
 
     return events;
 };
+
+function eventDateFilter(events){
+    var currentDate = new Date();
+    console.log(currentDate);
+
+    var eventSize = events.length;
+    for (var i = eventSize-1; i >= 0; i--){
+        eventDate = events[i][1][0].split('/');
+        eventDate[1]--;
+        eventDate[0]++;
+        eventDate = new Date(eventDate[2], eventDate[1], (eventDate[0]));
+        if (eventDate<currentDate){
+            events.splice(i, 1);
+        }
+
+    }
+    return events;
+
+}
