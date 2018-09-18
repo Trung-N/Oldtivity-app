7069bf98b2b372c1de61839341a28e1edf13ff82
TheRealBiggySmalls <nsujecki@student.unimelb.edu.au>
2018-09-18 17:13:41 +1000

    Just added some fun code to randomise colours of elements added.

--- Oldivity/app/src/main/java/com/example/oldivity/EventsAdapter.java
+++ Oldivity/app/src/main/java/com/example/oldivity/EventsAdapter.java
@@ -1,19 +1,23 @@
 package com.example.oldivity;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHolder> {
     private List<Event> events;
+    private String[] colours = {"#a8c0fc", "#fffdaf","#FFE5CE","#E4BD9C","#E94D3C","#E8925F","#92BFC1","#E57A80","#EFE0A3"};
 
     //copied from online to be changed later
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -24,8 +28,8 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
         public MyViewHolder(View itemView) {
             super(itemView);
 
-            nameTextView = (TextView) itemView.findViewById(R.id.contact_name);
-            messageButton = (Button) itemView.findViewById(R.id.message_button);
+            nameTextView = (TextView) itemView.findViewById(R.id.eventListName);
+            messageButton = (Button) itemView.findViewById(R.id.eventListButton);
         }
     }
 
@@ -57,11 +61,19 @@ public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.MyViewHold
         TextView textView = myViewHolder.nameTextView;
         textView.setText(event.getTitle());
         Button button = myViewHolder.messageButton;
-        button.setText("Join");
+        button.setText("View");
+        //randomises how each view looks
+        myViewHolder.itemView.setBackgroundColor(getRandomColour());
     }
 
     @Override
     public int getItemCount() {
         return events.size();
+    }
+
+    //randomises event colours to look nice
+    public int getRandomColour(){
+        int index = new Random().nextInt(colours.length);
+        return Color.parseColor(colours[index]);
     }
 }
