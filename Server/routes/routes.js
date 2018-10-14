161ce3cbfdf9e7e50daf9941722b99937f750e86
Doan Minh Trung Nguyen <trungn1@student.unimelb.edu.au>
2018-10-15 03:02:45 +1100

    add km lable

--- Server/routes/routes.js
+++ Server/routes/routes.js
@@ -2,7 +2,8 @@ const controller = require('../controller/userController.js');
 
 module.exports = function(app) {
 
+    app.post('/explore',controller.eventsearch);
     app.post('/eventsearch',controller.eventsearch);
     app.post('/event',controller.event);
-    app.post('/events',controller.events);
+    app.post('/myevents',controller.events);
 };
